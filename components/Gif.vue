3e27c18bf9fb2225d85b69de9075c86bf9e184eb
elisejakob <hei@elisejakob.no>
2019-06-09 12:18:02 +0200

    add smaller gifs and toggle button

--- components/Gif.vue
+++ components/Gif.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="gif" :class="{ visible: isVisible }">
-    <img :src="gif" />
+  <div>
+    <div class="gif" :class="{ visible: isVisible }">
+      <img :src="gif" />
+    </div>
+    <div class="gifbutton" @click="isVisible = !isVisible"></div>
   </div>
 </template>
 
@@ -12,29 +15,29 @@ export default {
   },
   data: function() {
     return {
-      gif: '/gifs/hi.gif',
+      gif: '/gifs/small/hi.gif',
       isVisible: false
     }
   },
   mounted() {
     if (this.holiday === 'christmas') {
-      this.gif = '/gifs/hi.gif';
+      this.gif = '/gifs/small/hi.gif';
     } else if (this.holiday === 'valentines') {
-      this.gif = '/gifs/kiss.gif';
+      this.gif = '/gifs/small/kiss.gif';
     } else if (this.holiday === 'halloween') {
-      this.gif = '/gifs/hi.gif';
+      this.gif = '/gifs/small/hi.gif';
     } else if (this.holiday === 'birthday') {
-      this.gif = '/gifs/birthday.gif';
+      this.gif = '/gifs/small/birthday.gif';
     } else if (this.holiday === 'nationalday') {
-      this.gif = '/gifs/cute.gif';
+      this.gif = '/gifs/small/cute.gif';
     } else if (this.day === 5) {
-      this.gif = '/gifs/love.gif';
+      this.gif = '/gifs/small/love.gif';
     }
     this.isVisible = true;
     setTimeout(()=>{
       this.isVisible = false;
       console.log('poop')
-    }, 24000)
+    }, 3600)
   }
 }
 </script>
@@ -46,15 +49,15 @@ export default {
   bottom: 0;
   right: 1rem;
   width: 20%;
-  max-width: 240px;
+  max-width: 200px;
   z-index: 1000;
 
   transform: translateY(300px);
-  transition: transform .8s linear;
+  transition: transform .5s linear;
 
   &.visible {
     transform: translateY(0);
-    transition: transform .8s linear;
+    transition: transform .5s linear;
   }
 
   img {
@@ -62,5 +65,13 @@ export default {
     width: 100%;
     vertical-align: bottom;
   }
+}
+.gifbutton {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  width: 10px;
+  height: 10px;
+  cursor: pointer;
 }
 </style>
--- components/Greeting.vue
+++ components/Greeting.vue
@@ -16,7 +16,7 @@ export default {
   },
   data: function() {
     return {
-      message: 'Soon happened!',
+      message: null,
     }
   },
   methods: {
